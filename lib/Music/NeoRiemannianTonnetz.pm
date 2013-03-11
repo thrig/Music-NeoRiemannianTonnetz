72c13c3e6595a76dd46b741c8ebd2274d9182d5c
Jeremy Mates <jmates@cpan.org>
2013-03-11 04:53:42 +0000

    Add some necessary methods, tests, etc.

--- lib/Music/NeoRiemannianTonnetz.pm
+++ lib/Music/NeoRiemannianTonnetz.pm
@@ -9,20 +9,90 @@ use strict;
 use warnings;
 
 use Carp qw/croak/;
+use Scalar::Util qw/reftype/;
 
-our $VERSION = '0.01';
+our $VERSION = '0.10';
+
+my %TRANSFORMATIONS = (
+  'P' => sub { ... },
+  'R' => sub { ... },
+  'L' => sub { ... },
+  'N' => 'RLP', # Nebenverwandt
+  'S' => 'LPR',    # Slide
+  'H' => 'LPL',
+);
 
 ########################################################################
 #
 # SUBROUTINES
 
-# So perhaps a "transform" method or function that takes a pitch set
-# (must then audit pitch set if triad, identify the parts) and a list of
-# transformations to make e.g. 'P' or 'N' (that would expand to 'RLP')
-# or other arbitrary sequences.
+sub new {
+  my ( $class, %param ) = @_;
+  # TODO need "default handler" if lookup something in x that does not
+  # exist - throw exception, omit, sub for some named symbol
+  my $self = { x => \%TRANSFORMATIONS };
 
-sub techno {
-  (qw/tonn tz/) x (8 * (shift || 1))
+  # XXX or also code ref for key/value lookup, if table gets too big
+  if ( exists $param{x} ) {
+    croak 'x must be hash reference' unless ref $param{x} eq 'HASH';
+    $self->{x} = $param{x};
+  }
+
+  bless $self, $class;
+
+  return $self;
+}
+
+# Turns string of tokens (e.g. 'RLP') into a list of tasks. Returns
+# array reference of such tasks.
+sub taskify_tokens {
+  my ( $self, $tokens, $tasks ) = @_;
+  $tasks //= [];
+  $tokens = [ split '', $tokens ] if !defined reftype $tokens;
+
+  for my $t (@$tokens) {
+    if ( exists $self->{x}{$t} ) {
+      if ( ref $self->{x}{$t} eq 'CODE' ) {
+        push @$tasks, $self->{x}{$t};
+      } else {
+        $self->taskify_tokens( $self->{x}{$t}, $tasks );
+      }
+    } else {
+      # XXX this could instead be a default handler
+      croak "unimplemented transformation token '$t'";
+    }
+  }
+
+  return $tasks;
+}
+
+sub techno { shift; (qw/tonn tz/) x ( 8 * ( shift || 1 ) ) }
+
+sub transform {
+  my $self = shift;
+  croak 'need tokens and pitch set' if @_ < 2;
+  my $tokens = shift;
+  croak 'tokens must be defined' unless defined $tokens;
+
+  # Assume list of tasks (code refs to call) if array ref, otherwise try
+  # to generate such a list.
+  my $tasks;
+  if ( ref $tokens eq 'ARRAY' ) {
+    $tasks = $tokens;
+  } else {
+    eval { $tasks = $self->taskify_tokens($tokens) };
+    croak $@ if $@;
+  }
+
+  # TODO find root, third, fifth of the supplied pitch set (pass through
+  # anything else, so cluster chord lacking third or fifth might be a
+  # noop, regardless the tasks).
+
+  my $new_pset;
+
+  # TODO apply tasks
+
+  return $new_pset;
 }
 
 1;
@@ -35,9 +105,18 @@ Music::NeoRiemannianTonnetz - performs Riemann operations on triads
 =head1 SYNOPSIS
 
   use Music::NeoRiemannianTonnetz;
-  TODO
+  my $nrt = Music::NeoRiemannianTonnetz->new;
+
+  my $new_pitch_set = $nrt->transform('RLPP', [0, 4, 7]);
+
+  my $tasks = $nrt->taskify_tokens('LPR');
+  my $new_pitch_set = $nrt->transform($tasks, [0, 4, 7]);
 
 =head1 DESCRIPTION
+
+TODO
+
+=head1 METHODS
 
 TODO
 
@@ -48,9 +127,13 @@ in the latest version, check:
 
 L<http://github.com/thrig/Music-NeoRiemannianTonnetz>
 
+C<techno> is not a bug, though may bug some.
+
 =head1 SEE ALSO
 
-TODO
+Various other music modules by the author, for different views on music
+theory: L<Music::AtonalUtil>, L<Music::Canon>,
+L<Music::Chord::Positions>, and more.
 
 =head1 AUTHOR
 
--- t/Music-NeoRiemannianTonnetz.t
+++ t/Music-NeoRiemannianTonnetz.t
@@ -10,13 +10,29 @@ my $deeply = $@ ? \&is_deeply : \&eq_or_diff;
 
 BEGIN { use_ok('Music::NeoRiemannianTonnetz') }
 
-my @techno           = (qw/tonn tz/) x 8;
-my @even_more_techno = (qw/tonn tz/) x 32;
+my $Nrt = Music::NeoRiemannianTonnetz->new;
+isa_ok( $Nrt, 'Music::NeoRiemannianTonnetz' );
 
-$deeply->( [Music::NeoRiemannianTonnetz::techno], \@techno, 'tonn tz' );
-$deeply->(
-  [ Music::NeoRiemannianTonnetz::techno(4) ],
-  \@even_more_techno, 'even more tonn tz'
-);
+taskify_tokens();
+techno();
 
-plan tests => 3;
+sub taskify_tokens {
+  my $tasks = $Nrt->taskify_tokens('P');
+  is( scalar @$tasks, 1, 'single step task count' );
+  ok( ref $tasks->[0] eq 'CODE', 'task is code ref' );
+
+  $tasks = $Nrt->taskify_tokens('N');    # should expand to RLP
+  is( scalar @$tasks, 3, 'three step task count' );
+  is( scalar( grep { ref $_ eq 'CODE' ? 1 : () } @$tasks ),
+    3, 'three little code refs' );
+}
+
+sub techno {
+  my @techno           = (qw/tonn tz/) x 8;
+  my @even_more_techno = (qw/tonn tz/) x 32;
+
+  $deeply->( [ $Nrt->techno ],    \@techno,           'tonn tz' );
+  $deeply->( [ $Nrt->techno(4) ], \@even_more_techno, 'even more tonn tz' );
+}
+
+plan tests => 8;
